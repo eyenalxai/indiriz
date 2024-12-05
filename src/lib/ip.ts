87ffde8b176c020be56ec00940bbff2e86fb8091
Sax Dhiut <mail@iyxeyl.me>
2024-12-05 13:30:22 +0400

    feat: add logging for IP header retrieval process

--- src/lib/ip.ts
+++ src/lib/ip.ts
@@ -3,6 +3,10 @@ import type { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapte
 export const getUserIp = (headersList: ReadonlyHeaders) => {
 	const ipHeaderKeys = ["cf-connecting-ip", "x-forwarded-for"]
 
+	for (const [key, value] of headersList) {
+		console.log(`${key}: ${value}`)
+	}
+
 	for (const key of ipHeaderKeys) {
 		const value = headersList.get(key)
 		if (value) return value.split(",")[0].trim()
